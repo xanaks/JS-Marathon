6a49b12ec2bb2f40648474c5bac86241fc0c1c9a
xanaks <36263837+xanaks@users.noreply.github.com>
2020-08-22 14:12:10 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,60 @@
-const firstRow = prompt("Введите первое слово");
-const secondRow = prompt("Введите второе слово");
+const $btn = document.getElementById("btn-kick");
 
-function getRow(firstRow, secondRow) {
-  let first = [];
-  let second = [];
-  let compare = [];
-  for (let i = 0; i < firstRow.length; i++) {
-    const firstLetter = firstRow[i];
-    if (firstLetter.charAt(0) == "а" || firstLetter.charAt(0) == "А") {
-      first += firstLetter;
-    }
-  }
+const character = {
+  name: "Pickachu",
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById("health-character"),
+  elProgressBar: document.getElementById("progressbar-character"),
+};
 
-  for (let i = 0; i < secondRow.length; i++) {
-    const secondLetter = secondRow[i];
-    if (secondLetter.charAt(0) == "а" || secondLetter.charAt(0) == "А") {
-      second += secondLetter;
-    }
-  }
+const enemy = {
+  name: "Charmander",
+  defaultHP: 100,
+  damageHP: 100,
+  elHP: document.getElementById("health-enemy"),
+  elProgressBar: document.getElementById("progressbar-enemy"),
+};
 
-  if (first.length > second.length) {
-    compare = firstRow;
-  } else {
-    compare = secondRow;
-  }
-  return compare;
+$btn.addEventListener("click", function () {
+  console.log("Kick");
+  changeHP(random(20), character);
+  changeHP(random(20), enemy);
+});
+
+function init() {
+  console.log("Start Game!");
+  renderHP(character);
+  renderHP(enemy);
 }
 
-alert("В строке " + getRow(firstRow, secondRow) + " больше букв а(А)");
+function renderHP(person) {
+  renderHPLife(person);
+  renderProgressbarHP(person);
+}
+
+function renderHPLife(person) {
+  person.elHP.innerText = person.damageHP + "/" + person.defaultHP;
+}
+
+function renderProgressbarHP(person) {
+  person.elProgressBar.style.width = person.damageHP + "%";
+}
+
+function changeHP(count, person) {
+  if (person.damageHP < count) {
+    person.damageHP = 0;
+    alert("Бедный " + person.name + " проиграл бой!");
+    $btn.disabled = true;
+  } else {
+    person.damageHP -= count;
+  }
+
+  renderHP(person);
+}
+
+function random(num) {
+  return Math.ceil(Math.random() * num);
+}
+
+init();
